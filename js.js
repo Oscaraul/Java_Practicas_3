522fb9a23638b806837d7d8d429941ee15053e9e
Oscaraul <racsoluar_391@hotmail.com>
2020-08-20 06:56:30 -0500

    actualizar

--- js.js
+++ js.js
@@ -1,23 +1,40 @@
+/*
 var calculadora = (function (num1, num2) {
+    var resultado= 0
+
     function actualizarResultado(nuevoResultado) {
-       console.log(nuevoResultado)
+      resultado=nuevoResultado
+
     }
     return {
-        sumar: function () {
-            var resultado = num1 + num2
-            actualizarResultado(resultado)
-        },
-        restar: function () {
-            var resultado = num1 - num2
-            actualizarResultado(resultado)
-        },
-        multiplicar: function () {
-            var resultado = num1 * num2
-            actualizarResultado(resultado)
-        },
-        dividir: function () {
-            var resultado = num1 / num2
-            actualizarResultado(resultado)
-        }
+      sumar: function () {
+          var resultado = num1 + num2
+          actualizarResultado(resultado)
+      },
+      restar: function () {
+          var resultado = num1 - num2
+          actualizarResultado(resultado)
+      },
+      multiplicar: function () {
+          var resultado = num1 * num2
+          actualizarResultado(resultado)
+      },
+      dividir: function () {
+          var resultado = num1 / num2
+          actualizarResultado(resultado)
+      },
+      resultado: function () {
+          return resultado
+      }
+
     }
+})
+*/
+
+function ejecutaproceso(micallback) {
+  micallback('devuelve valor')
+}
+
+ejecutaproceso(function (valordevuelto) {
+  console.log('respondo con '+valordevuelto);
 })
