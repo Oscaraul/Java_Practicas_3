c2d1c81d5c363069c712f9cb48b2d068f705f0e9
Oscaraul <racsoluar_391@hotmail.com>
2020-09-01 06:23:19 -0500

    getElementByName

--- U3_L1_2.3. getElementsByClassName/js/selectores.js
+++
@@ -1,7 +0,0 @@
-var tarjetas_amarillas = document.getElementsByClassName('card-panel yellow');
-
-
-
-for (i = 0; i < tarjetas_amarillas.length; i++) {
-tarjetas_amarillas[i].getElementsByClassName('texto_tarjeta')[0].innerHTML="OSCAR ES GUAPO";  
-}
