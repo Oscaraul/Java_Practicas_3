36a31cd50ad9e1329270c284b2889ac58d890ba9
Oscaraul <racsoluar_391@hotmail.com>
2020-09-27 10:15:29 -0500

    Configurando_Botones

--- Evaluacion Final/codBase/js/app.js
+++ Evaluacion Final/codBase/js/app.js
@@ -8,18 +8,16 @@ signoMas: document.getElementById("mas"),
 
 uno: document.getElementById("1"),
 
-
-
 init: function () {
 calculadora.pantalla.textContent = "0"
 
-punto.addEventListener("click",function(){ alert("Presionando la tecla del punto") })
+this.punto.addEventListener("click",function(){ alert("Presionando la tecla del punto") })
 
-igual.addEventListener("click",function(){ alert("Presionando la tecla del igual") })
+this.igual.addEventListener("click",function(){ alert("Presionando la tecla del igual") })
 
-cero.addEventListener("click",function(){ alert("Presionando la tecla del cero") })
+this.cero.addEventListener("click",function(){ alert("Presionando la tecla del cero") })
 
-signoMas.addEventListener("click",function(){ alert("Presionando la tecla del mas") })
+this.signoMas.addEventListener("click",function(){ alert("Presionando la tecla del mas") })
 
 uno.addEventListener("click",function(){ alert("Presionando la tecla del uno") })
 
