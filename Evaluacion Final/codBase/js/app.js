d1d8cd00bc3980b6e9dbd44555b505c7b343df8b
Oscaraul <racsoluar_391@hotmail.com>
2020-10-04 12:40:28 -0500

    codificacionDeNumeros2

--- Evaluacion Final/codBase/js/app.js
+++ Evaluacion Final/codBase/js/app.js
@@ -1,3 +1,172 @@
+var calculador = {
+  punto: document.getElementById("punto"),
+  igual: document.getElementById("igual"),
+  mas: document.getElementById("mas"),
+  menos: document.getElementById("menos"),
+  por : document.getElementById("por"),
+  division: document.getElementById("dividido"),
+
+  cero: document.getElementById("0"),
+  uno: document.getElementById("1"),
+  dos: document.getElementById("2"),
+  tres: document.getElementById("3"),
+  cuatro: document.getElementById("4"),
+  cinco: document.getElementById("5"),
+  seis: document.getElementById("6"),
+  siete: document.getElementById("7"),
+  ocho: document.getElementById("8"),
+  nueve: document.getElementById("9"),
+
+  pantalla: document.getElementById("display"),
+
+  valoralmacenado: 0,
+
+  operacion:"",
+
+  init: function () {
+    this.cero.addEventListener("click", this.ceroEvent);
+    this.uno.addEventListener("click", this.unoEvent);
+    this.dos.addEventListener("click", this.dosEvent);
+    this.tres.addEventListener("click", this.tresEvent);
+    this.cuatro.addEventListener("click", this.cuatroEvent);
+    this.cinco.addEventListener("click", this.cincoEvent);
+    this.seis.addEventListener("click", this.seisEvent);
+    this.siete.addEventListener("click", this.sieteEvent);
+    this.ocho.addEventListener("click", this.ochoEvent);
+    this.nueve.addEventListener("click", this.nueveEvent);
+
+    this.punto.addEventListener("click", this.puntoevent);
+    this.igual.addEventListener("click", this.igualEvent);
+
+    calculador.pantalla.textContent = "0"
+  },
+
+  ceroEvent: function () {
+  if (calculador.pantalla.textContent != "0")
+    if (calculador.pantalla.textContent.length <= 7)
+      calculador.pantalla.textContent += "0";
+},
+
+  unoEvent: function () {
+  if (calculador.pantalla.textContent.length <= 7)
+    if (calculador.pantalla.textContent == "0")
+       calculador.pantalla.textContent = "1";
+    else
+    calculador.pantalla.textContent += "1";
+},
+
+  dosEvent: function () {
+  if (calculador.pantalla.textContent.length <= 7)
+    if (calculador.pantalla.textContent == "0")
+      calculador.pantalla.textContent = "2";
+    else
+    calculador.pantalla.textContent += "2";
+
+  },
+
+  tresEvent: function () {
+    if (calculador.pantalla.textContent.length<=7) {
+      if (calculador.pantalla.textContent=="0") {
+        calculador.pantalla.textContent="3";
+      }
+      else {
+        calculador.pantalla.textContent +="3";
+      }
+    }
+
+  },
+
+  cuatroEvent: function () {
+    if (calculador.pantalla.textContent.length<=7) {
+      if (calculador.pantalla.textContent=="0") {
+        calculador.pantalla.textContent="4";
+      }
+      else {
+        calculador.pantalla.textContent +="4";
+      }
+    }
+
+  },
+
+  cincoEvent: function () {
+    if (calculador.pantalla.textContent.length<=7) {
+      if (calculador.pantalla.textContent=="0") {
+        calculador.pantalla.textContent="5";
+      }
+      else {
+        calculador.pantalla.textContent +="5";
+      }
+    }
+
+  },
+
+  seisEvent: function () {
+    if (calculador.pantalla.textContent.length<=7) {
+      if (calculador.pantalla.textContent=="0") {
+        calculador.pantalla.textContent="6";
+      }
+      else {
+        calculador.pantalla.textContent +="6";
+      }
+    }
+
+  },
+
+  sieteEvent: function () {
+    if (calculador.pantalla.textContent.length<=7) {
+      if (calculador.pantalla.textContent=="0") {
+        calculador.pantalla.textContent="7";
+      }
+      else {
+        calculador.pantalla.textContent +="7";
+      }
+    }
+
+  },
+
+  ochoEvent: function () {
+    if (calculador.pantalla.textContent.length<=7) {
+      if (calculador.pantalla.textContent=="0") {
+        calculador.pantalla.textContent="8";
+      }
+      else {
+        calculador.pantalla.textContent +="8";
+      }
+    }
+
+  },
+
+  nueveEvent: function () {
+    if (calculador.pantalla.textContent.length<=7) {
+      if (calculador.pantalla.textContent=="0") {
+        calculador.pantalla.textContent="9";
+      }
+      else {
+        calculador.pantalla.textContent +="9";
+      }
+    }
+
+  },
+
+  puntoevent: function () {
+    if (calculador.pantalla.textContent.indexOf(".")  == -1)
+      if (calculador.pantalla.textContent.length <= 7)
+        if (calculador.pantalla.textContent == "0")
+        calculador.pantalla.textContent = "0.";
+    else
+    calculador.pantalla.textContent += ".";
+
+  },
+
+}
+
+calculador.init();
+
+
+
+
+
+/*
 var calculadora = {
 
 pantalla: document.getElementById("display"),
@@ -21,6 +190,17 @@ nueve:document.getElementById("9"),
 init: function () {
 calculadora.pantalla.textContent = "0"
 
+digitos: function primero() {
+  if (calculadora.pantalla.length <=8) {
+    calculadora.pantalla=pantalla+calculadora.pantalla.value
+  }
+
+}
+
+
+
+
+/*
 this.on.addEventListener("click",function(){calculadora.pantalla.innerHTML = "0" })
 
 this.cero.addEventListener("click",function(){
@@ -120,8 +300,11 @@ this.punto.addEventListener("click",function(){ alert("Presionando la tecla del 
 
 this.igual.addEventListener("click",function(){ alert("Presionando la tecla del igual") })
 
-}
+*/
 
-}
 
+
+
+/*
 calculadora.init();
+*/
