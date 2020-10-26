923069c7da686c4eb9930b7cece7daf6119b4f48
Oscaraul <racsoluar_391@hotmail.com>
2020-10-25 22:02:39 -0500

    Suma2

--- Evaluacion Final/codBase/js/app.js
+++ Evaluacion Final/codBase/js/app.js
@@ -193,8 +193,15 @@ var calculador = {
   suma: function () {
     this.operacion="+";
     this.operandoA=this.pantalla.innerHTML;
-  }
+    this.pantalla.innerHTML="0"
+  },
 
+  oIgual: function () {
+    if (this.operacion=="+") {
+      this.pantalla.innerHTML = this.operandoA + this.operandoB;
+
+    }
+  }
 }
 
 calculador.init();
